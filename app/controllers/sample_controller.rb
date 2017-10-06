201fe5cd524fc37e149292c263dd6ef69000a75f
Shu Mimura <shuu.mimura@gmail.com>
2017-10-06 22:39:11 +0900

    :rocket: initial commit

---
+++ app/controllers/sample_controller.rb
@@ -0,0 +1,29 @@
+class SampleController < ApplicationController
+  require 'net/http'
+  require 'uri'
+  require 'json'
+  def index
+    # person_speech = params[:text]
+    person_speech = "年齢はおいくつですか？"
+
+    value = `curl -X POST https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk \
+-F "apikey=ljwzlxEfZoYpKWzQfVQxOBZPpo1DquR8" \
+-F "query=#{person_speech}"`
+
+  nyasuo_responce = "#{eval(value)[:results][0][:reply]}"
+
+  input_text = nyasuo_responce.to_s unless nyasuo_responce.kind_of?(String)
+  uri  = URI.parse('https://hooks.slack.com/services/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')
+  # params = { text: "hogehoge" }
+  params = { text: input_text }
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  http.start do
+    request = Net::HTTP::Post.new(uri.path)
+    request.set_form_data(payload: params.to_json)
+    http.request(request)
+  end
+  return input_text
+
+  end
+end
---
+++ vendor/bundler/ruby/2.3.0/gems/ffi-1.9.18/ext/ffi_c/libffi-x86_64-darwin16/include/ffitarget.h
@@ -0,0 +1,1 @@
+/Users/shu-P/workspace/nyasuo-talk/nyasuo_talk/vendor/bundler/ruby/2.3.0/gems/ffi-1.9.18/ext/ffi_c/libffi/src/x86/ffitarget.h
