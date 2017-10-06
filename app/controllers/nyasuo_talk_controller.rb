53a9ff102d6d7cbfaab59e99a25535cd42f90f46
Shu Mimura <shuu.mimura@gmail.com>
2017-10-07 03:10:39 +0900

    use curl

--- app/controllers/nyasuo_talk_controller.rb
+++ app/controllers/nyasuo_talk_controller.rb
@@ -2,7 +2,6 @@ class SampleController < ApplicationController
   require 'net/http'
   require 'uri'
   require 'json'
-  require 'kconv'
   def nyasuo_talk
     # person_speech = params[:text]
     person_speech = params[:text]
@@ -18,7 +17,7 @@ class SampleController < ApplicationController
 
 #simsim api
   # response = Net::HTTP.get_response(URI.parse("http://sandbox.api.simsimi.com/request.p?key=6aea4fbd-7813-496f-9526-c1b0b5cfc9f2&lc=ja&text=#{Kconv.tosjis("おっぱい大好き？")}"))
-  value = `curl -X GET http://sandbox.api.simsimi.com/request.p?key=6aea4fbd-7813-496f-9526-c1b0b5cfc9f2\&lc=ja\&text=おっぱい大好き？ `
+  value = `curl -X GET http://sandbox.api.simsimi.com/request.p?key=6aea4fbd-7813-496f-9526-c1b0b5cfc9f2\&lc=ja\&text=#{"おっぱい大好き？".encode("Shift_JIS").to_s} `
   nyasuo_response = value
 
 
