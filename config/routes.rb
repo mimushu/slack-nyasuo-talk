fb650e9298d58d4cea589bcf84c8795d80fb8215
Shu Mimura <shuu.mimura@gmail.com>
2017-10-07 00:48:26 +0900

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post 'sample/nyasuo_talk' to "sample#nyasuo_talk"
+  post 'sample/nyasuo_talk' to 'sample#nyasuo_talk'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
