be0a633e65bbc39c4a3a6bd91a099534f767a793
Shu Mimura <shuu.mimura@gmail.com>
2017-10-07 00:42:00 +0900

    change routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post 'sample/nyasuo_bot' to "sample#nyasuo_bot"
+  post 'sample/nyasuo_talk' to "sample#nyasuo_talk"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
